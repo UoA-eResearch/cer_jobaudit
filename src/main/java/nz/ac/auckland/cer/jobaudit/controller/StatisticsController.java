7aa21a4b70c440c44aa65d22abd91fff3bdb0144
Martin Feller <martinfeller73@gmail.com>
2014-05-08 09:46:57 +1200

    only show the last year by default

--- src/main/java/nz/ac/auckland/cer/jobaudit/controller/StatisticsController.java
+++ src/main/java/nz/ac/auckland/cer/jobaudit/controller/StatisticsController.java
@@ -54,9 +54,11 @@ public class StatisticsController {
         if (formData == null || formData.getFirstMonth() == null) {
             // no choice has been made
             Calendar now = Calendar.getInstance();
+            Calendar oneYearBack = Calendar.getInstance();
+            oneYearBack.add(Calendar.YEAR, -1);
             formData = new StatisticsFormData();
-            formData.setFirstMonth(this.historyFirstMonth);
-            formData.setFirstYear(this.historyFirstYear);
+            formData.setFirstMonth(oneYearBack.get(Calendar.MONTH));
+            formData.setFirstYear(oneYearBack.get(Calendar.YEAR));
             formData.setLastMonth(now.get(Calendar.MONTH));
             formData.setLastYear(now.get(Calendar.YEAR));
             formData.setCategory("Researcher");            
